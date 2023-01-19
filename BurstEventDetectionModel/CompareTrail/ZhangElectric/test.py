9e62ebb0d6dc5a353c9b4b986bd604ecbfb390a9
HAUSTCSA Laboratory <77826105+haustcsa@users.noreply.github.com>
2023-01-19 08:00:53 +0800

    Delete BurstEventDetectionModel directory

--- BurstEventDetectionModel/CompareTrail/ZhangElectric/test.py
+++
@@ -1,9 +0,0 @@
-# -*--coding:utf-8 -*--
-# @Time : 2022/9/4 0004 16:59
-# @Author : BAY
-# @File : test.py
-# @Software : PyCharm
-import os
-
-increment_files = os.listdir(r'D:\workspace\pycharm\PaperTrail\results\word_increment')
-print(increment_files)
--- BurstEventDetectionModel/CompareTrail/__init__.py
+++
@@ -1,5 +0,0 @@
-# _*__coding:utf-8 _*__
-# @Time :2022/5/30 0030 21:26
-# @Author :bay
-# @File __init__.py.py
-# @Software : PyCharm
